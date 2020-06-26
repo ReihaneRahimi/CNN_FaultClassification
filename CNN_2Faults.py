56a190158c0dee4420c0343e11469036d08d8f0b
Reihane Rahimi <reihane_rahimi@yahoo.com>
2020-06-26 18:20:59 +0100

    Second Commit after revising Loss Function

--- CNN_2Faults.py
+++ CNN_2Faults.py
@@ -1,28 +1,32 @@
-import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.callbacks import TensorBoard
+# from tensorflow.keras.callbacks import TensorBoard
 import time
+import numpy as np
 from numpy import load
+import pickle
 
 NAME = "CNN_Classification_2Faults"
-tensorboard = TensorBoard(log_dir="logs\{}".format(NAME))
+# tensorboard = TensorBoard(log_dir="logs\{}".format(NAME))
 
 #to load the data
-X = load ('Xin.npy')
-y = load ('Yout.npy')
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
 
+pickle_in = open("y.pickle","rb")
+Y = pickle.load(pickle_in)
+Y = np.array (Y)
+
+input_shape=(100,100,1)
 
 model = Sequential()
 
-model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Conv2D(16, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(16, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -30,15 +34,14 @@ model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
 model.add(Dense(64))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(4))
+model.add(Activation('softmax'))
 
-model.compile(loss='binary_crossentropy',
+model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y,
+model.fit(X, Y,
           batch_size=32,
-          epochs=1,
-          validation_split=0.3,
-          callbacks=[tensorboard])
+          epochs=20,
+          validation_split=0.3)
